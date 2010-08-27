ebdf6369dbfbe8826e606a793d964ba829bfe314
Peter Donald <peter@realityforge.org>
2010-08-27 23:59:14 +1000

    Rework the spec_helper so it is similar to other buildr plugins

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,33 +1,34 @@
 require 'spec'
 
 DEFAULT_BUILDR_DIR=File.expand_path(File.dirname(__FILE__) + '/../../buildr')
-BUILDR_DIR=ENV['BUILDR_DIR'] || DEFAULT_BUILDR_DIR
-
-unless File.exist?("#{BUILDR_DIR}/buildr.gemspec")
-  raise "Unable to find buildr.gemspec in #{BUILDR_DIR == DEFAULT_BUILDR_DIR ? 'guessed' : 'specified'} $BUILD_DIR (#{BUILDR_DIR})"
-end
-
-require 'rubygems'
-
-# For testing we use the gem requirements specified on the buildr.gemspec
-Gem::Specification.load(File.expand_path("#{BUILDR_DIR}/buildr.gemspec", File.dirname(__FILE__))).
-    dependencies.each { |dep| gem dep.name, dep.requirement.to_s }
-
-# hook into buildr's spec_helpers load process
-unless defined?(SpecHelpers)
-  module SandboxHook
-    def SandboxHook.included(spec_helpers)
-      $LOAD_PATH.unshift(File.dirname(__FILE__))
-      $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-      require 'buildr_osgi_assembler'
+BUILDR_DIR =
+  begin
+    if ENV['BUILDR_DIR']
+      ENV['BUILDR_DIR']
+    elsif File.exist?(File.expand_path('../buildr_dir', __FILE__))
+      File.read(File.expand_path('../buildr_dir', __FILE__)).strip
+    else
+      DEFAULT_BUILDR_DIR
     end
   end
 
-  require "#{BUILDR_DIR}/spec/spec_helpers.rb"
+unless File.exist?("#{BUILDR_DIR}/buildr.gemspec")
+  raise "Unable to find buildr.gemspec in #{BUILDR_DIR == DEFAULT_BUILDR_DIR ? 'guessed' : 'specified'} $BUILDR_DIR (#{BUILDR_DIR})"
+end
 
-  module SpecHelpers
-    def runtime_gen_dir(project)
-      project._("target/generated/osgi_runtime")
-    end
+# hook into buildr's spec_helpers load process
+module SandboxHook
+  def SandboxHook.included(spec_helpers)
+    $LOAD_PATH.unshift(File.dirname(__FILE__))
+    $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+    require 'buildr_osgi_assembler'
+  end
+end
+
+require "#{BUILDR_DIR}/spec/spec_helpers.rb"
+
+module SpecHelpers
+  def runtime_gen_dir(project)
+    project._("target/generated/osgi_runtime")
   end
 end
